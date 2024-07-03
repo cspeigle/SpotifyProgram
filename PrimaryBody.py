9114494284be980d0b72ec8a14dd30d6fc6046d7
cspeigle <165057474+cspeigle@users.noreply.github.com>
2024-07-03 17:32:38 -0400

    Update PrimaryBody.py

--- PrimaryBody.py
+++ PrimaryBody.py
@@ -2,9 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 # Spotify API credentials
-client_id = 'd91a102c99ae4fee9205b7f2c23f37f1'
-client_secret = '11cecf5bbb8142e286d546fa24430140'
-redirect_uri = 'http://localhost:8888/callback'
+
 
 # Authentication scope
 scope = 'playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public'
